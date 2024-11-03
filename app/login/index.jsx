b624398f70f6a202e39a5ca0f0caecf1828949a8
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2024-11-03 21:07:29 +0530

    Clear Auth Start

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,9 +1,39 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useCallback, useEffect } from "react";
 import Colors from "../../constants/Colors";
-import { TouchableOpacity } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import { useOAuth } from "@clerk/clerk-expo";
+import * as Linking from "expo-linking";
+
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    // Warm up the android browser to improve UX
+    // https://docs.expo.dev/guides/authentication/#improving-user-experience
+    void WebBrowser.warmUpAsync();
+    return () => {
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
+};
+
+WebBrowser.maybeCompleteAuthSession();
 
 const LoginScreen = () => {
+  useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow({
+          redirectUrl: Linking.createURL("/home", { scheme: "myapp" }),
+        });
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
   return (
     <View style={{ flex: 1, backgroundColor: Colors.WHITE }}>
       <Image
@@ -33,6 +63,7 @@ const LoginScreen = () => {
       </View>
       <View style={{ flex: 1, justifyContent: "flex-end", marginBottom: 8 }}>
         <TouchableOpacity
+          onPress={onPress}
           style={{
             backgroundColor: Colors.PRIMARY,
             margin: 8,
